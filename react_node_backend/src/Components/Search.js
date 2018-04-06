957fe42ff147b3bb8fb6f9b402e97cf39da90f17
Vajid Kagdi <vajid9@gmail.com>
2018-04-05 23:00:12 -0700

    Search project component

--- react_node_backend/src/Components/Search.js
+++ react_node_backend/src/Components/Search.js
@@ -5,6 +5,7 @@ import Table from './Table'
 import { withRouter } from 'react-router-dom'
 import axios from 'axios'
 import swal from 'sweetalert2'
+import { envURL, reactURL } from '../config/environment';
 
 class Search extends Component {
     constructor(props) {
@@ -13,27 +14,40 @@ class Search extends Component {
             action: 'search',
             tableHeaderData: ['Title', 'Employer', 'Skill', 'Avg Bid', 'Budget Range', 'Budget Period', 'No. of Bids', 'Status'],
             tableRowData: [],
-            paginationData: []
-
+            paginationData: [],
+            status:''
         };
     }
 
+    handleUserInput = (e) => {
+        const value = e.target.value;
+        this.setState({
+            status:value
+        });
+        this.getSearchProjects();
+    }
     componentWillMount() {
-        let url = 'http://localhost:3001/isLoggedIn';
-        axios.get(url,{withCredentials: true})
+        this.getSearchProjects();
+    }
+
+    getSearchProjects(){
+        let url = envURL+'isLoggedIn';
+        axios.get(url, { withCredentials: true })
             .then(res => {
                 if (res.data.responseCode === 0) {
                     localStorage.setItem('id', res.data.id);
                     localStorage.setItem('name', res.data.name);
                     localStorage.setItem('email', res.data.email);
-                    let getSearchProjects = 'http://localhost:3001/searchProject';
+                    let getSearchProjects = envURL+'searchProject';
                     let id = localStorage.getItem('id');
                     let parameter = window.location.href;
                     let search_string = parameter.split('=')[1]
+                    let status_filter = this.state.status;
                     if (id) {
                         var apiPayload = {
                             id: id,
-                            search_string:search_string
+                            search_string: search_string,
+                            status_filter:status_filter
                         };
                         axios.post(getSearchProjects, apiPayload)
                             .then(res => {
@@ -50,7 +64,7 @@ class Search extends Component {
                                     this.setState({
                                         paginationData: new Array(Math.ceil(list.length / chuckSize)).fill().map(_ => list.splice(0, chuckSize))
                                     });
-                                    var initialDisplay = this.state.paginationData[0]
+                                    var initialDisplay = this.state.paginationData[0]?this.state.paginationData[0]:[]
                                     this.setState({
                                         tableRowData: initialDisplay
                                     });
@@ -63,19 +77,21 @@ class Search extends Component {
                             .catch(err => {
                                 console.error(err);
                             });
-                    }            }
+                    }
+                }
                 else {
                     swal({
                         type: 'error',
                         title: 'Login',
                         text: 'Login Required',
-                      })
+                    })
                     this.props.history.push('/login')
                 }
             })
             .catch(err => {
                 console.error(err);
             });
+
     }
     handlePageClick(e) {
         e.preventDefault();
@@ -104,7 +120,7 @@ class Search extends Component {
             <div class='container'>
                 <Header />
 
-            
+
                 <div class="content-wrapper mt-1">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item">
@@ -118,8 +134,16 @@ class Search extends Component {
                             <div class="col-lg-11 container">
                                 <div class="">
                                     <div>
-                                        <Table action={this.state.action} tableHeaderData={this.state.tableHeaderData} tableRowData={this.state.tableRowData} />
 
+                                        <select class="float-right form-control col-sm-2 mb-3" name="expYear"
+                                            value={this.state.status} onChange={this.handleUserInput} >
+                                            <option value="OPEN">Open</option>
+                                            <option value="Closed">Closed</option>
+                                            <option value="">ALL</option>
+                                        </select>
+                                        <h4><strong class='float-right text-info mr-4'>Filter</strong></h4>
+                                        
+                                        <Table action={this.state.action} tableHeaderData={this.state.tableHeaderData} tableRowData={this.state.tableRowData} />
                                     </div>
                                 </div>
                             </div>
